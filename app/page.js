9def94e5b78d3fe06d2beaf28ac5428453d35308
ahmed elrazky <ahmedelrazky94@gmail.com>
2024-11-17 19:46:59 +0200

    final

--- app/page.js
+++ app/page.js
@@ -4,13 +4,17 @@ import { useState } from "react";
 
 export default function Home() {
   const emptyArr = ["", "", "", "", "", "", "", "", ""];
+
   //empty array to store board values
   const [tic, changeTic] = useState(emptyArr)
+
   //state to change based on the player turn
   const [player, setPlayer] = useState(null)
+
   //state for player who win the game
   const [win, setWinner] = useState(null)
-  //board records state
+
+  //states to store board records
   const [xWin, changeX] = useState(0)
   const [oWin, changeO] = useState(0)
   const [draw, setDraw] = useState(0)
@@ -72,24 +76,24 @@ export default function Home() {
   }
 
   return (
-    <main>
+    <main className="">
       <div className="py-[3%] max-md:py-[15%]">
-        <h1 className="text-6xl text-center font-black">TicTacToe</h1>
+        <h1 className="text-6xl text-center font-black text-black">TicTacToe</h1>
       </div>
       <div className="flex justify-center items-center">
-        <div className="flex justify-center align-middle items-center h-2/3 w-1/2 max-md:w-2/3 flex-wrap cell-border">
+        <div style={{border:"3px solid #000"}} className="flex justify-center align-middle items-center h-2/3 w-1/2 max-md:w-2/3 flex-wrap">
           {
             tic.map((tic, id) => (
-              <div key={id} onClick={() => { changePlayer(id); }} className="cell-border h-[150px] max-md:h-[100px] w-1/3 flex justify-center items-center text-5xl"> {tic}
+              <div key={id} onClick={() => { changePlayer(id); }} style={{border:"3px solid #000"}} className="cell-border h-[150px] max-md:h-[100px] w-1/3 flex justify-center items-center text-5xl"> <p className={`${(tic==="X"?"text-red-600" : "text-blue-600")}`}>{tic}</p>
               </div>
             ))
           }
         </div>
       </div>
 
-      {win == null && <div className="text-center py-4 text-3xl"> Turn of : ( {player == null ? "X" : player} ) </div>}
+      {win == null && <div className="text-center py-4 text-3xl text-gray-800"> Turn of : <span className="text-fuchsia-600">( {player == null ? "X" : player} )</span> </div>}
       {win != null && <div className="text-center py-4 text-3xl">( {win} ) win <button className="text-red-600" onClick={() => { changeTic(emptyArr); setWinner(null); }}>click to restart game</button> </div>}
-      {<p className="text-center py-2 text-3xl">X wins : {xWin} times & O wins : {oWin} times & draw : {draw} times </p>}
+      {<p className="text-center py-2 text-3xl text-gray-600"><span className="text-red-600">X wins :  {xWin} & </span><span className="text-blue-600"> O wins : {oWin}</span> <span className="text-green-600"> & draw : {draw}</span> </p>}
 
     </main>
   );
